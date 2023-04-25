dc00795a8c7f4ce4ee04def8e97001628bf628df
TabbieManywanda <achiengtabitha@gmail.com>
2023-04-25 11:06:39 +0000

    Edit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,6 +8,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world() -> str:
-    return 'Hello World'
+@app.route('/', methods = ['GET', 'POST'])
+def hello_world():
+    '''Returns html template with Hello World'''
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
