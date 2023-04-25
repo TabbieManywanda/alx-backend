16f4d99e65b67ed45e182401ec537b2326f5add7
TabbieManywanda <achiengtabitha@gmail.com>
2023-04-25 11:21:27 +0000

    Another Edit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world() -> str:
     '''Returns html template with Hello World'''
     return render_template('0-index.html')
