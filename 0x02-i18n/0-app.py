280d9af54f85651602345f40414c6a2e67124675
TabbieManywanda <achiengtabitha@gmail.com>
2023-04-25 11:14:17 +0000

    Edit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,16 +2,17 @@
 
 '''Setting up a basic Flask app'''
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET', 'POST'])
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def hello_world() -> str:
     '''Returns html template with Hello World'''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
